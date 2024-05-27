caef2c16e6eb4a3029a7f4acc65918e71cb21208
Felipe Mendes <you@example.com>
2024-05-27 14:33:01 -0300

    Kunakushimikishimikimo lukitokatakizikatemo

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO.Compression;
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
+﻿Console.WriteLine("--- Nome Ninja ---\n");
+
+Console.ResetColor();
+
+Console.Write("Digite seu nome: ");
+string nome = Console.ReadLine()!;
+
+string nomeNinja = nome.Trim().ToLower()
+    .Replace("a", "KA")
+    .Replace("b", "ZU")
+    .Replace("c", "MI")
+    .Replace("d", "TE")
+    .Replace("e", "KU")
+    .Replace("f", "LU")
+    .Replace("g", "JI")
+    .Replace("h", "RI")
+    .Replace("i", "KI")
+    .Replace("j", "ZUS")
+    .Replace("k", "ME")
+    .Replace("l", "TA")
+    .Replace("m", "RIN")
+    .Replace("n", "TO")
+    .Replace("o", "MO")
+    .Replace("p", "NO")
+    .Replace("q", "KE")
+    .Replace("r", "SHI")
+    .Replace("s", "ARI")
+    .Replace("t", "CHI")
+    .Replace("u", "DO")
+    .Replace("v", "RU")
+    .Replace("w", "MEI")
+    .Replace("x", "NA")
+    .Replace("y", "FU")
+    .Replace("z", "ZI");
+
+nomeNinja = $"{nomeNinja.Substring(0, 1).ToUpper()}{nomeNinja.Substring(1).ToLower()}";
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+Console.WriteLine($"\nSeu nome ninja é {nomeNinja}.");
